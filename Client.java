0bf752353a232219735fa796b5f16612af7ec966
aaubin649 <aaubin649@students.fairmontschools.com>
2025-05-30 08:21:30 -0700

    Updated main

--- Client.java
+++ Client.java
@@ -1,255 +1,103 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Client {
-    // Global static input variable
-    static ArrayList<ArrayList<Integer>> A = new ArrayList<>(Arrays.asList(
-        new ArrayList<>(Arrays.asList(1, 0, 0, 1, 0, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 1, 0)),
-        new ArrayList<>(Arrays.asList(9, 0, 0, 1, 0, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(0, 0, 0, 1, 2, 0, 0, 0)),
-        new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 1, 1, 1))
-    ));
+
+    static List<List<Integer>> A = Arrays.asList(
+        Arrays.asList(1, 0, 0, 0, 1),
+        Arrays.asList(1, 1, 1, 0, 1),
+        Arrays.asList(0, 0, 1, 1, 1),
+        Arrays.asList(0, 0, 0, 0, 1),
+        Arrays.asList(1, 1, 1, 1, 1)
+    );
 
     public static void main(String[] args) {
-        runTest();
+        execute();
     }
 
-    public static void runTest() {
-        int rows = A.size();
-        int cols = A.get(0).size();
-        // boolean[][] visited = new boolean[rows][cols];
-        ArrayList<String> path = new ArrayList<>();
-        String startingEdge = "";
-        String endingEdge = "";
-        String currentDirection = "";
-        boolean didTurn = false;
-
-        boolean found = false;
-        for (int i = 0; i < rows && !found; i++) {
-            for (int j = 0; j < cols && !found; j++) {
-                if (A.get(i).get(j) == 1 && isEdge(i, j, rows, cols)) {
-                    ArrayList<String> tempPath = new ArrayList<>();
-                    boolean[][] tempVisited = new boolean[rows][cols];
-                    if (dfs(i, j, A, tempVisited, tempPath, startingEdge, endingEdge, didTurn, currentDirection)) {
-                        if (isEdgeCoord(tempPath.get(0), rows, cols) &&
-                            isEdgeCoord(tempPath.get(tempPath.size() - 1), rows, cols) &&
-                            !tempPath.get(0).equals(tempPath.get(tempPath.size() - 1))) {
-                            path = tempPath;
-                            System.out.println("Current Path: "+path);
-                            found = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println("Path coordinates:");
-        System.out.println(path);
-        printPathMap(path, rows, cols);
+    public static void execute() {
+        List<String> route = tracePath(A);
+        System.out.println("Final Path Coordinates:");
+        System.out.println(route);
+        System.out.println("Map View of Path:");
+        renderPath(A, route);
     }
 
-    public static boolean dfs(int row, int col, ArrayList<ArrayList<Integer>> A, boolean[][] visited, ArrayList<String> path, String startingEdge, String endingEdge, boolean didTurn, String currentDirection) {
-        int rows = A.size();
-        int cols = A.get(0).size();
+    public static List<String> tracePath(List<List<Integer>> map) {
+        int rows = map.size();
+        int cols = map.get(0).size();
+        List<int[]> entries = new ArrayList<>();
 
-        if (row < 0 || col < 0 || row >= rows || col >= cols) return false;
-        if (A.get(row).get(col) != 1 || visited[row][col]) return false;
+        for (int r = 0; r < rows; r++) {
+            if (map.get(r).get(0) == 1) entries.add(new int[]{r, 0});
+            if (map.get(r).get(cols - 1) == 1) entries.add(new int[]{r, cols - 1});
+        }
 
-        visited[row][col] = true;
-        path.add("A[" + row + "][" + col + "]");
-        
+        for (int c = 0; c < cols; c++) {
+            if (map.get(0).get(c) == 1) entries.add(new int[]{0, c});
+            if (map.get(rows - 1).get(c) == 1) entries.add(new int[]{rows - 1, c});
+        }
 
-        // check where previous path and current path is to determine direction
-        if (path.size() == 2){
-            int prevRow = Integer.parseInt(path.get(path.size() - 2).split("\\[|\\]")[1]);
-            int prevCol = Integer.parseInt(path.get(path.size() - 2).split("\\[|\\]")[3]);
-
-            // going left or right
-            if (prevRow == row){
-                if (prevCol < col){
-                    // going right
-                    startingEdge = "Left";
-                    System.out.println("Starting on Left edge");
-                }
-                else{
-                    // going left
-                    startingEdge = "Right";
-                    System.out.println("Starting on Right edge");
-                }
-            }
-            else{
-                // going up or down
-                if(prevRow < row){
-                    // going down
-                    startingEdge = "Top";
-                    System.out.println("Starting on Top edge");
-                }
-                else{
-                    // going up
-                    startingEdge = "Down";
-                    System.out.println("Starting on Bottom edge");
-                }
+        for (int[] start : entries) {
+            boolean[][] seen = new boolean[rows][cols];
+            List<String> path = new ArrayList<>();
+            int edge = identifyEdge(start[0], start[1], rows, cols);
+            if (search(map, seen, start[0], start[1], edge, path, null, 0)) {
+                return path;
             }
         }
 
-        if(path.size() > 2 && didTurn){
-            if(isEdge(row,col,rows,cols)){
-                if(row == 0){
-                    if(col == 0){
-                        endingEdge = "Top";
-                        endingEdge = "Left";
-                    }
-                    else if(col == cols - 1){
-                        endingEdge = "Top";
-                        endingEdge = "Right";
-                    }
-                    else{
-                        endingEdge = "Top";
-                    }
-                }
-                else if(row == rows - 1){
-                    if(col == 0){
-                        endingEdge = "Bottom";
-                        endingEdge = "Left";
-                    }
-                    else if(col == cols - 1){
-                        endingEdge = "Bottom";
-                        endingEdge = "Right";
-                    }
-                    else{
-                        endingEdge = "Bottom";
-                    }
-                }
-                else if(col == 0){
-                    endingEdge = "Left";
-                }
-                else if(col == cols - 1){
-                    endingEdge = "Right";
-                }
+        return Collections.emptyList();
+    }
 
+    private static boolean search(List<List<Integer>> map, boolean[][] seen, int r, int c, int originEdge, List<String> path, int[] prevDir, int turns) {
+        int rows = map.size(), cols = map.get(0).size();
+        if (r < 0 || r >= rows || c < 0 || c >= cols || seen[r][c] || map.get(r).get(c) != 1)
+            return false;
+
+        seen[r][c] = true;
+        path.add("A[" + r + "][" + c + "]");
+        int currentEdge = identifyEdge(r, c, rows, cols);
+
+        if (path.size() > 1 && currentEdge != -1 && currentEdge != originEdge && turns >= 1)
+            return true;
+
+        int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int[] dir : moves) {
+            int updatedTurns = turns;
+            if (prevDir != null && (dir[0] != prevDir[0] || dir[1] != prevDir[1])) {
+                updatedTurns++;
             }
-        }
-
-        // update current direction to be the direction of last coordinate and current coordinate
-        if (path.size() > 2){
-            int prevRow = Integer.parseInt(path.get(path.size() - 2).split("\\[|\\]")[1]);
-            int prevCol = Integer.parseInt(path.get(path.size() - 2).split("\\[|\\]")[3]);
-
-            // going left or right
-            if (prevRow == row){
-                if (prevCol < col){
-                    // going right
-                    if(!currentDirection.equals("") && !currentDirection.equals("Right")){
-                        System.out.println("Did turn");
-                        didTurn = true;
-                    }
-                    currentDirection = "Right";
-                    System.out.println("Going right");
-                }
-                else{
-                    // going left
-                    if(!currentDirection.equals("") && !currentDirection.equals("Left")){
-                        System.out.println("Did turn");
-                        didTurn = true;
-                    }
-                    currentDirection = "Left";
-                    System.out.println("Going left");
-                }
-            }
-            else{
-                // going up or down
-                if(prevRow < row){
-                    // going down
-                    if(!currentDirection.equals("") && !currentDirection.equals("Down")){
-                        System.out.println("Did turn");
-                        didTurn = true;
-                    }
-                    currentDirection = "Down";
-                    System.out.println("Going down");
-                }
-                else{
-                    // going up
-                    if(!currentDirection.equals("") && !currentDirection.equals("Top")){
-                        System.out.println("Did turn");
-                        didTurn = true;
-                    }
-                    currentDirection = "Top";
-                    System.out.println("Going up");
-                }
-            }
-        }
-
-        
-        
-        // check if we have reach an edge and the edge is not the same as the starting edge and not opposite the starting edge
-        if ( isEdge(row, col, rows, cols) && path.size() > 2) {
-            System.out.println("Found edge at: A[" + row + "][" + col + "]");
-            if (startingEdge.equals("Top") && (endingEdge.equals("Left") || endingEdge.equals("Right") )) {
-                System.out.println("Found path from " + startingEdge + " to " + endingEdge);
-                return true;
-            } else if (startingEdge.equals("Bottom") && (endingEdge.equals("Left") || endingEdge.equals("Right"))) {
-                System.out.println("Found path from " + startingEdge + " to " + endingEdge);
-                return true;
-            } else if (startingEdge.equals("Left") && (endingEdge.equals("Top") || endingEdge.equals("Bottom"))) {
-                System.out.println("Found path from " + startingEdge + " to " + endingEdge);
-                return true;
-            } else if (startingEdge.equals("Right") && (endingEdge.equals("Top") || endingEdge.equals("Bottom"))) {
-                System.out.println("Found path from " + startingEdge + " to " + endingEdge);
+            if (search(map, seen, r + dir[0], c + dir[1], originEdge, path, dir, updatedTurns)) {
                 return true;
             }
         }
 
-        
-
-        int[][] directions = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-        for (int[] dir : directions) {
-            int newRow = row + dir[0];
-            int newCol = col + dir[1];
-            dfs(newRow, newCol, A, visited, path, startingEdge, endingEdge, didTurn, currentDirection);
-        }
-
-        return true;
+        path.remove(path.size() - 1);
+        return false;
     }
 
-    public static boolean isEdge(int row, int col, int rows, int cols) {
-        return row == 0 || row == rows - 1 || col == 0 || col == cols - 1;
+    private static int identifyEdge(int r, int c, int rows, int cols) {
+        if (r == 0) return 0;
+        if (r == rows - 1) return 1;
+        if (c == 0) return 2;
+        if (c == cols - 1) return 3;
+        return -1;
     }
 
-    public static boolean isEdgeCoord(String coord, int rows, int cols) {
-        String[] parts = coord.split("\\[|\\]");
-        int r = Integer.parseInt(parts[1]);
-        int c = Integer.parseInt(parts[3]);
-        return isEdge(r, c, rows, cols);
-    }
+    public static void renderPath(List<List<Integer>> map, List<String> path) {
+        int rows = map.size(), cols = map.get(0).size();
+        String[][] view = new String[rows][cols];
+        for (String[] row : view) Arrays.fill(row, " ");
 
-    public static void printPathMap(ArrayList<String> path, int rows, int cols) {
-        String[][] display = new String[rows][cols];
-
-        // Initialize all cells with blank space
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                display[i][j] = " ";
-
-        // Mark path coordinates with "1"
         for (String coord : path) {
-            String[] parts = coord.split("\\[|\\]");
-            int r = Integer.parseInt(parts[1]);
-            int c = Integer.parseInt(parts[3]);
-            display[r][c] = "1";
+            int r = Integer.parseInt(coord.substring(coord.indexOf('[') + 1, coord.indexOf(']')));
+            int c = Integer.parseInt(coord.substring(coord.lastIndexOf('[') + 1, coord.lastIndexOf(']')));
+            view[r][c] = "1";
         }
 
-        // Print the display map
-        for (int i = 0; i < rows; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j < cols; j++) {
-                System.out.print(display[i][j]);
-                if (j < cols - 1) System.out.print(", ");
-            }
-            System.out.println(" ]");
+        for (String[] row : view) {
+            System.out.println(Arrays.toString(row));
         }
     }
 }
